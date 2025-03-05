6aa5e2630dce3bfd2b92441e8fd09f0faa7c1884
pashka1837 <pashka1837@yandex.kz>
2025-03-05 15:28:32 +0700

    feat: useHook package added

--- src/components/MatchFilter/MatchFilter.tsx
+++ src/components/MatchFilter/MatchFilter.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { MatchStatuses } from "../../constants";
 import { FilterBtn } from "./FilterBtn";
 import { DropDownMenu } from "./DropDownMenu";
+import { useClickAway } from "@uidotdev/usehooks";
 
 type MatchFilterProps = {
   setFilter: React.Dispatch<React.SetStateAction<MatchStatuses | "All">>;
@@ -10,11 +11,19 @@ type MatchFilterProps = {
 
 export function MatchFilter({ setFilter, filter }: MatchFilterProps) {
   const [open, setOpen] = useState(false);
+  const ref = useClickAway(() =>
+    setOpen(false)
+  ) as React.RefObject<HTMLDivElement | null>;
   return (
     <div className="relative text-text-second z-10">
       <FilterBtn open={open} setOpen={setOpen} filter={filter} />
       {open && (
-        <DropDownMenu setFilter={setFilter} filter={filter} setOpen={setOpen} />
+        <DropDownMenu
+          ref={ref}
+          setFilter={setFilter}
+          filter={filter}
+          setOpen={setOpen}
+        />
       )}
     </div>
   );
